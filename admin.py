6684c875000d8acfc1e12bd0b395398a30c9378d
Popa Cosmin <poparobertcosmin@gmail.com>
2021-11-29 21:27:35 +0200

    prima faza

---
+++ admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+# from .models import Profile
+
+# @admin.register(Profile)
+# class ProfileAdmin(admin.ModelAdmin):
+# 	list_display = ['',]
+# 	list_filter = ('',)
+# 	list_editable = ('',)
+# 	prepopulated_fields = {'slug': ('name',)}
---
+++ urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from {{ app_name}} import views
+
+app_name = "{{ app_name}}"
+
+# urlpatterns = [
+#     path("", view.HomeView.as_view(), name="home"),
+# ]
